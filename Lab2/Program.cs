3462461b6809d65b505f1874a45d66905cc5dadc
yaromchik <r3187575r@yandex.ru>
2022-03-15 20:39:03 +0300

    тест комит

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,7 +10,7 @@ namespace Lab1
     {
         /// <summary>
         ///  The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
