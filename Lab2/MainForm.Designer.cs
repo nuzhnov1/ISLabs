9534724e09155c75f93f810e9e091d112237c30b
sunman <andrey_nuzhnov.01@mail.ru>
2022-03-15 10:46:52 +0700

    Создал проект Lab2 из проекта Lab1

---
+++ Lab2/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Lab1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer Components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (Components != null))
+            {
+                Components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.InitButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ExecButton = new System.Windows.Forms.Button();
+            this.QueryBox = new System.Windows.Forms.TextBox();
+            this.QueryLabel = new System.Windows.Forms.Label();
+            this.TableView = new System.Windows.Forms.DataGridView();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StatusValueLabel = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.TableView)).BeginInit();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InitButton
+            // 
+            this.InitButton.AutoSize = true;
+            this.InitButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.InitButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.InitButton.FlatAppearance.BorderSize = 2;
+            this.InitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InitButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.InitButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.InitButton.ImageKey = "(нет)";
+            this.InitButton.Location = new System.Drawing.Point(10, 10);
+            this.InitButton.Margin = new System.Windows.Forms.Padding(10);
+            this.InitButton.Name = "InitButton";
+            this.InitButton.Size = new System.Drawing.Size(250, 50);
+            this.InitButton.TabIndex = 8;
+            this.InitButton.Text = "Инициализировать соединение";
+            this.InitButton.UseVisualStyleBackColor = false;
+            this.InitButton.Click += new System.EventHandler(this.InitButtonClick);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.AutoSize = true;
+            this.OpenButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.OpenButton.Enabled = false;
+            this.OpenButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.OpenButton.FlatAppearance.BorderSize = 2;
+            this.OpenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OpenButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.OpenButton.ImageKey = "(нет)";
+            this.OpenButton.Location = new System.Drawing.Point(10, 80);
+            this.OpenButton.Margin = new System.Windows.Forms.Padding(10);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(250, 50);
+            this.OpenButton.TabIndex = 9;
+            this.OpenButton.Text = "Открыть соединение";
+            this.OpenButton.UseVisualStyleBackColor = false;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButtonClick);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.CloseButton.Enabled = false;
+            this.CloseButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.CloseButton.FlatAppearance.BorderSize = 2;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CloseButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.CloseButton.ImageKey = "(нет)";
+            this.CloseButton.Location = new System.Drawing.Point(280, 10);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(10);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(250, 50);
+            this.CloseButton.TabIndex = 10;
+            this.CloseButton.Text = "Закрыть соединение";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButtonClick);
+            // 
+            // ExecButton
+            // 
+            this.ExecButton.AutoSize = true;
+            this.ExecButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ExecButton.Enabled = false;
+            this.ExecButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.ExecButton.FlatAppearance.BorderSize = 2;
+            this.ExecButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExecButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ExecButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ExecButton.ImageKey = "(нет)";
+            this.ExecButton.Location = new System.Drawing.Point(280, 80);
+            this.ExecButton.Margin = new System.Windows.Forms.Padding(10);
+            this.ExecButton.Name = "ExecButton";
+            this.ExecButton.Size = new System.Drawing.Size(250, 50);
+            this.ExecButton.TabIndex = 11;
+            this.ExecButton.Text = "Выполнить команду";
+            this.ExecButton.UseVisualStyleBackColor = false;
+            this.ExecButton.Click += new System.EventHandler(this.ExecuteButtonClick);
+            // 
+            // QueryBox
+            // 
+            this.QueryBox.AcceptsReturn = true;
+            this.QueryBox.AcceptsTab = true;
+            this.QueryBox.AllowDrop = true;
+            this.QueryBox.Enabled = false;
+            this.QueryBox.HideSelection = false;
+            this.QueryBox.Location = new System.Drawing.Point(550, 68);
+            this.QueryBox.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.QueryBox.Multiline = true;
+            this.QueryBox.Name = "QueryBox";
+            this.QueryBox.Size = new System.Drawing.Size(450, 84);
+            this.QueryBox.TabIndex = 13;
+            this.QueryBox.Click += new System.EventHandler(this.EnviromentClick);
+            this.QueryBox.Leave += new System.EventHandler(this.QueryBoxLostFocus);
+            // 
+            // QueryLabel
+            // 
+            this.QueryLabel.AutoSize = true;
+            this.QueryLabel.Location = new System.Drawing.Point(549, 49);
+            this.QueryLabel.Margin = new System.Windows.Forms.Padding(10, 10, 10, 0);
+            this.QueryLabel.Name = "QueryLabel";
+            this.QueryLabel.Size = new System.Drawing.Size(167, 19);
+            this.QueryLabel.TabIndex = 14;
+            this.QueryLabel.Text = "Команда на языке SQL:";
+            this.QueryLabel.Click += new System.EventHandler(this.EnviromentClick);
+            // 
+            // TableView
+            // 
+            this.TableView.AllowUserToOrderColumns = true;
+            this.TableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TableView.Location = new System.Drawing.Point(10, 172);
+            this.TableView.Margin = new System.Windows.Forms.Padding(10);
+            this.TableView.Name = "TableView";
+            this.TableView.RowTemplate.Height = 25;
+            this.TableView.Size = new System.Drawing.Size(988, 360);
+            this.TableView.TabIndex = 15;
+            this.TableView.Visible = false;
+            this.TableView.Click += new System.EventHandler(this.EnviromentClick);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StatusLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StatusLabel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.StatusLabel.Location = new System.Drawing.Point(550, 10);
+            this.StatusLabel.Margin = new System.Windows.Forms.Padding(10, 10, 0, 10);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(142, 19);
+            this.StatusLabel.TabIndex = 16;
+            this.StatusLabel.Text = "Статус соединения:";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StatusLabel.Click += new System.EventHandler(this.EnviromentClick);
+            // 
+            // StatusValueLabel
+            // 
+            this.StatusValueLabel.AutoSize = true;
+            this.StatusValueLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StatusValueLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StatusValueLabel.ForeColor = System.Drawing.Color.Black;
+            this.StatusValueLabel.Location = new System.Drawing.Point(692, 10);
+            this.StatusValueLabel.Margin = new System.Windows.Forms.Padding(0, 10, 10, 10);
+            this.StatusValueLabel.Name = "StatusValueLabel";
+            this.StatusValueLabel.Size = new System.Drawing.Size(161, 19);
+            this.StatusValueLabel.TabIndex = 17;
+            this.StatusValueLabel.Text = "не инициализировано";
+            this.StatusValueLabel.Click += new System.EventHandler(this.EnviromentClick);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.InitButton);
+            this.MainPanel.Controls.Add(this.TableView);
+            this.MainPanel.Controls.Add(this.StatusValueLabel);
+            this.MainPanel.Controls.Add(this.ExecButton);
+            this.MainPanel.Controls.Add(this.StatusLabel);
+            this.MainPanel.Controls.Add(this.OpenButton);
+            this.MainPanel.Controls.Add(this.QueryLabel);
+            this.MainPanel.Controls.Add(this.CloseButton);
+            this.MainPanel.Controls.Add(this.QueryBox);
+            this.MainPanel.Location = new System.Drawing.Point(9, 9);
+            this.MainPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1011, 552);
+            this.MainPanel.TabIndex = 18;
+            this.MainPanel.Click += new System.EventHandler(this.EnviromentClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1029, 570);
+            this.Controls.Add(this.MainPanel);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "ИС. Лабораторная работа №1. Команда - HydraSquad.";
+            ((System.ComponentModel.ISupportInitialize)(this.TableView)).EndInit();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InitButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button ExecButton;
+        private System.Windows.Forms.TextBox QueryBox;
+        private System.Windows.Forms.Label QueryLabel;
+        private System.Windows.Forms.DataGridView TableView;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label StatusValueLabel;
+        private System.Windows.Forms.Panel MainPanel;
+    }
+}
---
+++ Lab2/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Lab1
+{
+    public partial class MainForm : Form
+    {
+        private readonly ConnectionForm ConnectionForm;
+        private readonly NpgsqlConnection Connection;
+        private string ConnectionString;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.ConnectionForm = new ConnectionForm();
+            this.Connection = new NpgsqlConnection();
+            this.AddOwnedForm(ConnectionForm);
+            this.ConnectionString = "";
+        }
+
+        private void InitButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                this.ConnectionForm.ShowDialog();
+                this.ConnectionString = this.ConnectionForm.ConnectionString;
+            }
+            catch (SystemException exception)
+            {
+                MessageBox.Show(
+                    exception.Message, "Ошибка инициализации",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+            }
+
+            if (this.ConnectionString == "")
+            {
+                this.OpenButton.Enabled = false;
+                this.CloseButton.Enabled = false;
+                this.ExecButton.Enabled = false;
+                this.QueryBox.Enabled = false;
+                this.TableView.Visible = false;
+
+                this.StatusValueLabel.Text = "не инициализировано";
+                this.StatusValueLabel.ForeColor = Color.Black;
+            }
+            else
+            {
+                try
+                {
+                    this.Connection.ConnectionString = this.ConnectionString;
+                }
+                catch (NpgsqlException exception)
+                {
+                    MessageBox.Show(
+                        exception.Message, "Ошибка инициализации",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error
+                    );
+                }
+
+                this.OpenButton.Enabled = true;
+                this.CloseButton.Enabled = false;
+                this.ExecButton.Enabled = false;
+                this.QueryBox.Enabled = false;
+                this.TableView.Visible = false;
+
+                this.StatusValueLabel.Text = "инициализировано";
+                this.StatusValueLabel.ForeColor = Color.Green;
+            }
+        }
+
+        private async void OpenButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                await this.Connection.OpenAsync();
+
+                this.InitButton.Enabled = false;
+                this.OpenButton.Enabled = false;
+                this.CloseButton.Enabled = true;
+                this.ExecButton.Enabled = this.QueryBox.Text != "";
+                this.QueryBox.Enabled = true;
+                this.TableView.Visible = true;
+
+                this.StatusValueLabel.Text = "открыто";
+                this.StatusValueLabel.ForeColor = Color.Yellow;
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show(
+                    exception.Message, "Ошибка открытия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private async void CloseButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                await this.Connection.CloseAsync();
+
+                this.StatusValueLabel.Text = "закрыто";
+                this.StatusValueLabel.ForeColor = Color.Red;
+
+                this.InitButton.Enabled = true;
+                this.OpenButton.Enabled = true;
+                this.CloseButton.Enabled = false;
+                this.ExecButton.Enabled = false;
+                this.QueryBox.Enabled = false;
+                this.TableView.Visible = false;
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show(
+                    exception.Message, "Ошибка закрытия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private async void ExecuteButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                await using var query = new NpgsqlCommand(this.QueryBox.Text, this.Connection);
+                await using var reader = await query.ExecuteReaderAsync();
+                
+                if (reader.FieldCount > 0)
+                {
+                    var table = new DataTable();
+
+                    table.Load(reader);
+                    this.TableView.DataSource = table;
+                }
+                else
+                {
+                    MessageBox.Show("Операция выполнена успешно", 
+                        "Результат выполнения команды", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+                }
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка выполнения команды", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void EnviromentClick(object sender, EventArgs e) => ((Control)sender).Select();
+        private void QueryBoxLostFocus(object sender, EventArgs e) => this.ExecButton.Enabled = this.QueryBox.Text != "";
+    }
+}
